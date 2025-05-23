0ef726a6405043bc5c609d8051e13c6cec10dab9
Satwik Sanjeev <satwiksanjeev3@gmail.com>
2025-05-23 23:14:01 +0530

    vercel updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,22 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  // 👇 This is important for routing to work on Vercel
+  base: '/',
+})
